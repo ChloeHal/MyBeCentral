179f492b104f00e9be162ecdc9d8be4dfd741960
Nico-Capy <nicolacorradini97@yahoo.com>
2023-03-23 10:39:42 +0100

    Coucou, je suis un commit

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -3,13 +3,8 @@ import logo from "./assets/logo.png";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-black h-8 flex justify-items-stretch items-center">
+    <nav className="bg-black h-8 flex items-center">
       <img className="h-4" src={logo} alt="Logo" />
-      <input
-        type="text"
-        className="h-5 justify-center border border-white-300 rounded-md bg-transparent"
-        placeholder="Search"
-      />
 
       <img
         className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
