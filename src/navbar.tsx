6a7be0b9e0cbad19df41dfa8b4ebdf3df45fe54a
Nico-Capy <nicolacorradini97@yahoo.com>
2023-03-23 11:27:29 +0100

    Fini navbar

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -8,7 +8,7 @@ const Navbar: React.FC = () => {
       <img className="h-7" src={logo} alt="Logo" />
 
       <img
-        className="inline-block h-7 w-7 rounded-full ring-2 ring-white"
+        className="inline-block h-7 w-7 m-2 rounded-full ring-2 ring-white"
         src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
         alt=""
       />
