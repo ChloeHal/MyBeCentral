37a35f47cd7f4859f7015e1d22e74a6729913261
Nico-Capy <nicolacorradini97@yahoo.com>
2023-03-24 12:37:01 +0100

    Enfin fini

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -17,7 +17,7 @@ const Navbar: React.FC = () => {
           <img
             className="absolute h-4 w-4 sm:h-5 sm:w-5 right-2 top-2 sm:right-3 sm:top-3"
             src={Search}
-            alt="Search"
+            alt="Search icon"
           />
         </div>
       </form>
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
       <img
         className="inline-block h-7 w-7 m-2 rounded-full ring-2 ring-white"
         src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt="Profile"
+        alt="John Doe's profile"
       />
     </nav>
   );
