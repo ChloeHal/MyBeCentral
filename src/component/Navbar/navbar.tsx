8ae64453bb715b2c7f930ee6cf08ebf0562a4686
Chloé halloin <halloinchloe@gmail.com>
2023-03-29 10:06:28 +0200

    /

--- src/component/Navbar/navbar.tsx
+++ src/component/Navbar/navbar.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom"; // importez Link depuis react-router-do
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="bg-black h-12 flex items-center p-2">
+    <nav className="fixed top-0 left-0 right-0 bg-black h-12 flex items-center p-2 z-10">
       <img
         className="h-7 m-2 lg:h-8 lg:m-3 xl:h-9 xl:m-4 2xl:h-9 2xl:m-5"
         src={Logo}
