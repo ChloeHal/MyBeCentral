8698d4c08dd8e470a37cca87e3c4254e20f6fc84
louisevst <louisevassart@gmail.com>
2023-03-31 11:21:41 +0200

    added companies with all descriptions

--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -1,21 +1,20 @@
-import React from "react";
 import RoundButton from "../component/Button/RoundButton";
 import arrow from "../content/arrow.svg";
-import profile from "../content/backbecode.png";
-import logo from "../content/becode.png";
 import back from "../content/newbackblue.png";
 import { useNavigate, useParams } from "react-router-dom";
 import companies from "../assets/backend/companies";
 import { useState, useEffect } from "react";
+import Button from "../component/Button/Button";
+import { Translation, useTranslation } from "react-i18next";
 interface Company {
   id: number;
   name: string;
   picture: string;
   logo: string;
   description: string;
-  followers: number;
 }
 function Company() {
+  const { t } = useTranslation();
   const { companyname } = useParams();
   const [company, setCompany] = useState<Company | null>(null);
 
@@ -32,6 +31,7 @@ function Company() {
   const navigateFeed = () => {
     navigate("/feed");
   };
+
   return company ? (
     <div className="h-screen flex items-center justify-center relative bg-whitish dark:bg-black dark:text-whitish">
       <div className="absolute z-50 w-20 h-20 md:w-24 md:h-24">
@@ -41,9 +41,9 @@ function Company() {
         clickHandler={navigateFeed}
         classname="absolute z-50 w-12 h-12 bg-black/50 md:w-12 md:h-12 top-0 left-0 mt-4 ml-4"
         color=""
-        svg={<img src={arrow} className="h-1/2 " />}
+        svg={<img src={arrow} className="h-1/2 -translate-x-[2px]" />}
       />
-      <div className="flex flex-col h-screen lg:flex-row">
+      <div className="flex flex-col h-screen lg:flex-row w-full">
         <div className="h-1/2 relative lg:h-full lg:w-1/2 shadow-2xl ">
           <img src={company.picture} className="w-full h-full object-cover" />
         </div>
@@ -56,14 +56,29 @@ function Company() {
               <h1 className="text-center">{company.name}</h1>
             </div>
           </div>
-          <div className="overflow-y-scroll mx-6 md:w-2/3 md:text-l">
-            <p>{company.description}</p>
+          <div className="overflow-y-auto mx-6 md:w-2/3 md:text-l">
+            <p className="text-center">{company.description}</p>
           </div>
         </div>
       </div>
     </div>
   ) : (
-    <div>Company not found</div>
+    <div
+      style={{ backgroundImage: `url(${back})` }}
+      className="h-screen bg-whitish dark:bg-dark dark:text-whitish lg:bg-right bg-right-bottom max-w-full flex justify-center items-center flex-col bg-cover bg-no-repeat"
+    >
+      <main className="text-center">
+        <h1 className="text-[60px]">404</h1>
+        <p className="leading-6">{t("404.label")}</p>
+        <p>{t("404p.label")}</p>
+      </main>
+      <Button
+        name={t("goback.label")}
+        color="teal"
+        class="m-8"
+        clickHandler={navigateFeed}
+      />
+    </div>
   );
 }
 
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -29,10 +29,10 @@ const User: React.FC = () => {
         clickHandler={navigateFeed}
         classname="absolute z-50 w-12 h-12 bg-black/50 md:w-12 md:h-12 top-0 left-0 mt-4 ml-4"
         color=""
-        svg={<img src={arrow} className="h-1/2 " />}
+        svg={<img src={arrow} className="h-1/2 -translate-x-[2px]" />}
       />
       <div className="flex flex-col h-screen lg:flex-row bg-whitish dark:bg-dark dark:text-whitish">
-        <div className="h-1/2 relative lg:h-full lg:w-1/2 shadow-2xl ">
+        <div className="h-1/2 relative lg:h-full lg:w-1/2 shadow-2xl w-full">
           <img src={profile} className="w-full h-full object-cover" />
         </div>
 
@@ -40,21 +40,20 @@ const User: React.FC = () => {
           style={{ backgroundImage: `url(${back})` }}
           className="h-1/2 flex flex-col bg-center bg-cover justify-center items-center relative lg:w-1/2 lg:h-full"
         >
-          <div className="flex flex-row my-4">
-            <p className="text-center px-4 md:text-2xl">34 followers</p>
-            <p className="text-center px-4 md:text-2xl">36 follows</p>
+          <div className="flex justify-between my-4">
+            <p className="text-center px-8 md:text-2xl">34 followers</p>
+            <p className="text-center px-8 md:text-2xl">36 follows</p>
           </div>
-          <div className="flex flex-row my-4 mx-6 w-2/3">
-            <div className="flex py-auto w-1/3 px-4">
-              <img src={logo} className="object-cover" />
-            </div>
-            <div className="flex flex-col w-2/3 px-4 justify-center h-full md:text-xl">
+          <div className="flex  justify-center items-center my-4 mx-6 w-2/3">
+            <img src={logo} className="object-cover w-1/3 sm:w-1/4" />
+
+            <div className="flex flex-col px-4 justify-center h-full md:text-xl">
               <h1>Pépé PHP</h1>
               <p>BeCode</p>
             </div>
           </div>
-          <div className="my-4 overflow-y-scroll mx-6 md:w-2/3 md:text-l">
-            <p>
+          <div className="my-4 overflow-y-auto mx-6 md:w-2/3 md:text-l">
+            <p className="text-center">
               Lorem ipsum dolor sit amet. Hic ipsam fugit et sunt ullam aut enim
               quis non voluptas asperiores et accusantium sint. Id accusantium
               internos quo ducimus omnis hic voluptates nemo ut unde obcaecati
